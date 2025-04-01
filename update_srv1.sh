c256fd74baeeaf5f862dbc674a56793a28068041
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2025-03-31 22:27:17 -0300

    Update update_srv1.sh

--- update_srv1.sh
+++ update_srv1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo teste
+wget https://raw.githubusercontent.com/tmferreira-ti/SOII/main/Confs/srv1/sysctl.conf -O /etc/sysctl.conf --no-check-certificate --no-cache
+#
+wget https://raw.githubusercontent.com/tmferreira-ti/SOII/main/Confs/srv1/interfaces -O /etc/network/interfaces --no-check-certificate --no-cache
+wget https://raw.githubusercontent.com/tmferreira-ti/SOII/main/Confs/srv1/nftables.conf -O /etc/nftables.conf --no-check-certificate --no-cache
+
+systemctl enable nftables
+reboot
+
