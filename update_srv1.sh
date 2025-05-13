1112c75de57fdc72633845c47c7b831be902f796
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2025-05-12 22:04:26 -0300

    Update update_srv1.sh

--- update_srv1.sh
+++ update_srv1.sh
@@ -5,5 +5,17 @@ wget https://raw.githubusercontent.com/tmferreira-ti/SOII/main/Confs/srv1/interf
 wget https://raw.githubusercontent.com/tmferreira-ti/SOII/main/Confs/srv1/nftables.conf -O /etc/nftables.conf --no-check-certificate --no-cache
 
 systemctl enable nftables
+
+apt update 
+
+apt install bind9 -y
+
+wget https://raw.githubusercontent.com/tmferreira-ti/SOII/refs/heads/main/Confs/srv1/dns/named.conf.local -O /etc/bind/named.conf.local
+
+https://raw.githubusercontent.com/tmferreira-ti/SOII/refs/heads/main/Confs/srv1/dns/named.conf.options -O /etc/bind/named.conf.options
+
+mkdir -p /etc/bind/domains/fatecads/
+
+wget https://raw.githubusercontent.com/tmferreira-ti/SOII/refs/heads/main/Confs/srv1/dns/db.fatecads.edu.br -O /etc/bind/domains/fatecads/db.fatecads.edu.br
 reboot
 
